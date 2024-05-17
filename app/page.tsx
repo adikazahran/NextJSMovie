2a3f32ee8d2e81d21acbdd773901e3a2da0b4398
zaradika <nicesvan@gmail.com>
2024-05-17 09:54:03 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style = {{fontSize : "24px", color : "blue"}}>Home Movie List</h1>
+      <h2 style = {{fontSize : "20px", color: "green"}}>Muhammad Adika Zahran</h2>
+      <h3 style = {{fontSize : "18px", color: "red"}}>Kelas Pemrograman Web 2024</h3>
+    </div>
+  );
+
+  }
